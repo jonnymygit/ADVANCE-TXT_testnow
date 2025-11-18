0a362e323bce7ddbafbee6b88d41159367e9611e
jonnymygit <jonnyjames9022@gmail.com>
2025-11-19 00:16:04 +0530

    Update test_resolve.py

--- test_resolve.py
+++ test_resolve.py
@@ -1,12 +1,6 @@
-import asyncio
-from classplus_resolver import resolve_classplus_url
+from resolver import resolve_classplus
 
-async def t():
-    url = "https://media-cdn.classplusapp.com/1005566/cc/acc5916a6f7a4a5d9b22281817eca927-mg/master.m3u8"
-    try:
-        r = await resolve_classplus_url(url)
-        print("Resolved:", r)
-    except Exception as e:
-        print("Failed:", e)
+link = "https://media-cdn.classplusapp.com/1005566/cc/acc5916a6f7a4a5d9b22281817eca927-mg/master.m3u8"
 
-asyncio.run(t())
+signed = resolve_classplus(link)
+print("Signed URL:", signed)
